32bab3b81a3c9b6e025cc6600cada43adb3e6bf5
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-20 18:20:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,9 +8,10 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads =>{
       :pieces =>4,
+      :rent_in_dollars=>{}
+      :names=>{}
     }
   }
-
 end
 
 def monopoly_with_third_tier
